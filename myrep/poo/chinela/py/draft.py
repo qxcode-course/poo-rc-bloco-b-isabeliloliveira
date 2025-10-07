3b196cc39da859a663e7b84f8feb4da0220d8177
isabeliloliveira <isabeli.oliveira@alu.ufc.br>
2025-10-07 14:56:07 +0000

    summit

---
+++ myrep/poo/chinela/py/draft.py
@@ -0,0 +1,19 @@
+class Chinela:
+    def __init__(self, tamanho: int):
+        self.tamanho: int = 0 
+        
+
+    def set_tamanho(self):
+        # self.min_tam: int = 20
+        # self.max_tam: int = 50
+
+
+def main():
+    # chinela: Chinela=Chinela(" ")
+    while True:
+        line: str=input()
+        args: list[str]= line.split(" ") 
+
+
+
+main()
