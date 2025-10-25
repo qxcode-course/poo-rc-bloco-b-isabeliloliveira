e99bb4342831e1fc218e2ee919bd219b998754c8
isabeliloliveira <isabeli.oliveira@alu.ufc.br>
2025-10-25 02:58:16 +0000

    summit

--- myrep/poo/grafite/py/draft.py
+++ myrep/poo/grafite/py/draft.py
@@ -1,7 +1,8 @@
 class Grafite:
     def __init__(self):
         self.__size: int = 0
-        
+        self.thickness: int
+        self.hardness: str
         
 class Lapiseira:
     def __init__(self):
@@ -11,3 +12,7 @@ class Lapiseira:
         if self.__size!=None:
             return True
         
+    def usagePerSheet(self):
+        
+    def __str__(self):
+        return f"calibre: {}, grafite: [{}:{}:{}]"
