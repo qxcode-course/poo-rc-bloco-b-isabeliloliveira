6ea653edbeb81ebc63e85cf968849f00a4cd1937
isabeliloliveira <isabeli.oliveira@alu.ufc.br>
2025-10-28 14:51:28 +0000

    summit

--- myrep/poo/charger/py/draft.py
+++ myrep/poo/charger/py/draft.py
@@ -21,13 +21,19 @@ class Bateria:
     
     def desc(self, temp: int):
         self.__carga-=temp
-        if self.carga>self.__cap:
+        if self.__carga>self.__cap:
             self.__carga = 0
-            
         
-            
+    def carreg(self, temp: int):
+        self.__carga += temp
+        if self.__carga > self.__cap:
+            self.__carga = self.__cap
+        
+    def __str__(self):
+        return f"({self.__carga}/{self.__cap})"
+                      
     
-    
-    
+
 class Carregador:
-    
+    def __init__(self):
+        self.ligado = False
