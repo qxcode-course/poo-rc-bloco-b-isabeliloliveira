f657108cb450ffd0c9fa0f1b02de5994690b8227
isabeliloliveira <isabeli.oliveira@alu.ufc.br>
2025-10-28 13:40:51 +0000

    summit

--- myrep/poo/grafite/py/draft.py
+++ myrep/poo/grafite/py/draft.py
@@ -37,15 +37,47 @@ class Grafite:
     
 class Lapiseira:
     def __init__(self):
-        self.tip = Grafite | None = None
+        self.__tip = Grafite | None = None
         self.__thickness: float = 0
-        
-    def hasGrafite(self):
-        if self.__size!=None:
-            return True
         
     def set_thickness(self, espessura: float):
         self.__thickness = espessura
+        
+    def get_tip(self):
+        return self.__tip
+    
+    def inserir(self, grafite: Grafite):
+        if self.__tip !=None:
+            print("fail: ja existe grafite")
+            return
+        
+        if grafite.get_thickness() != self.__thickness:
+            print("fail: calibre incompativel")
+            return
+        self.__tip = grafite
+    
+    def remover(self) -> Grafite | None:
+        aux = self.__tip
+        self.__tip = None
+        return aux
+        
+    def writePage(self):
+        if self.__tip is None:
+            print("fail: nao existe grafite")
+            return
+        size = self.__tip.get_size()
+        cost = self.__tip.usagePerSheet()
+        result=size-cost
+        
+        if size<=10:
+            print("fail: tamanho insuficiente")
+            return
+        if result<10:
+            print("fail: folha incompleta")
+            self.__tip.set_size(10)
+            return
+
+        self.__tip.set_size(result)
         
     def __str__(self):
         grafite = self.__tip if self.__tip != None else "null"
@@ -54,6 +86,23 @@ class Lapiseira:
     
     
 def main():
-    
-    
+    lapiseira = Lapiseira()
+    while True:
+        line = input()
+        print("$"+line)
+        args = line.split(" ")
+        if args[0]=="end":
+            break
+        elif args[0]=="show":
+            print(lapiseira)
+        
+        elif args[0]=="insert":
+            thickness = args[1]
+            hardness = args [2]
+            size = args[3]
+            grafite = Grafite(float(thickness), hardness, int(size))
+        elif args [0] =="init":
+            lapiseira.set_thickness(float(args[1]))
+        
+        
 main()
