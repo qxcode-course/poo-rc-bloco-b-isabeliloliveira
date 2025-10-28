97aecf88d7b9f7af3f2f13332178744a47a02977
isabeliloliveira <isabeli.oliveira@alu.ufc.br>
2025-10-28 10:55:28 +0000

    summit

--- myrep/poo/grafite/py/draft.py
+++ myrep/poo/grafite/py/draft.py
@@ -1,9 +1,40 @@
 class Grafite:
-    def __init__(self):
-        self.__size: int = 0
-        self.thickness: int
-        self.hardness: str
+    def __init__(self, thickness: float, hardness: str, size: int):
+        self.__size = size
+        self.__thickness = thickness
+        self.__hardness = hardness
+    
+    def usagePerSheet(self):
+        if self.__hardness == "HB":
+            return 1
         
+        elif self.__hardness == "2B":
+            return 2
+        
+        elif self.__hardness == "4B":
+            return 4
+        
+        elif self.__hardness == "6B":
+            return 6
+        
+        else:
+            return 0
+        
+    def get_thinckness(self):
+        return self.__thickness
+    
+    def get_hardness(self):
+        return self.__hardness
+    
+    def get_size(self):
+        return self.__size
+    
+    def set_size(self, size: int):
+        self.__size = size
+
+    def __str__(self):
+        return f"[{self.__thickness}:{self.__hardness}:{self.__size}]"
+    
 class Lapiseira:
     def __init__(self):
         self.tip = None
@@ -11,8 +42,14 @@ class Lapiseira:
     def hasGrafite(self):
         if self.__size!=None:
             return True
-        
-    def usagePerSheet(self):
+    
         
     def __str__(self):
-        return f"calibre: {}, grafite: [{}:{}:{}]"
+        return f"calibre: {self.__thickeness}, grafite: [{}:{}:{}]"
+    
+    
+    
+def main():
+    
+    
+main()
